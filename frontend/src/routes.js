40c6bb8db39c52f8b2313d3cbbb626e50d376fe6
gomzalo <201318652@ingenieria.usac.edu.gt>
2021-12-28 20:04:57 -0600

    docker-compose.yml fixed jeje

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -15,10 +15,10 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
 */
-import Dashboard from "views/Dashboard.js";
+import Inicio from "views/Inicio";
 import TableList from "views/TableList.js";
 import UserProfile from "views/UserProfile.js";
-import Inicio from "views/Inicio";
+import Dashboard from "views/Dashboard.js";
 
 var routes = [
   {
